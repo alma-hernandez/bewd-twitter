40b6ee5bdaa59456ff0422a555d4a9653ffce73e
alma-hernandez <1984ahern@gmail.com>
2024-01-30 09:41:33 -0600

    json for users updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,12 @@ class UsersController < ApplicationController
   def create
     user_params = params.require(:user).permit(:username, :email, :password)
 
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
-      render json: {
-        user: {
-          username: user.username,
-          email: user.email
-        }
-      }
+    if @user.save
+      render 'create', status: :created
     else
-      render json: { error: user.errors.full_messages.join(', ') }, status: :unprocessable_entity
+      render json: { error: @user.errors.full_messages.join(', ') }, status: :unprocessable_entity
     end
   end
 
